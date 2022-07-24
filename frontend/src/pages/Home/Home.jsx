903e02070fdeb14baf3341e89518c01e265206b0
hitesh <hitesh22rana@gmail.com>
2022-07-25 00:07:46 +0530

    Home Page --changes

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "./Home.module.scss";
 import { useNavigate } from "react-router-dom";
+
+import Navigation from "../../components/shared/Navigation/Navigation";
 import Button from "../../components/shared/Button/Button";
 
 const Home = () => {
@@ -11,24 +13,28 @@ const Home = () => {
     }
 
     return (
-        <section>
-            <div className={styles.wrapper}>
-                <div className={styles.leftWrapper}>
-                    <h1>Welcome to KodeNest!</h1>
-                    <p className={styles.text}>
-                        We’re working hard to get KodeNest ready for everyone!
-                        While we wrap up the finishing touches, we’re adding
-                        people gradually to make sure nothing breaks.
-                    </p>
-                    <div>
-                        <Button onClick={startRegister} text="Let's Go" />
+        <>
+            <Navigation />
+            <section>
+                <div className={styles.wrapper}>
+                    <div className={styles.leftWrapper}>
+                        <h1>Welcome to KodeNest!</h1>
+                        <p className={styles.text}>
+                            We’re working hard to get KodeNest ready for
+                            everyone! While we wrap up the finishing touches,
+                            we’re adding people gradually to make sure nothing
+                            breaks.
+                        </p>
+                        <div>
+                            <Button onClick={startRegister} text="Let's Go" />
+                        </div>
+                    </div>
+                    <div className={styles.rightWrapper}>
+                        <img src="/images/hero.png" alt="hero" />
                     </div>
                 </div>
-                <div className={styles.rightWrapper}>
-                    <img src="/images/hero.png" alt="hero" />
-                </div>
-            </div>
-        </section>
+            </section>
+        </>
     );
 };
 
